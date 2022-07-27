5bfb4128ca9571f9390f7a9f949af0a666e8e150
Cassio Sandrigo Santini <cassio.santini@educacao.sp.gov.br>
2022-07-27 13:15:47 -0300

    fazendo alterações para melhor visualização no mobile

--- Dia4/day4.js
+++ Dia4/day4.js
@@ -7,8 +7,8 @@ function resolver() {
     let numero = Math.floor(Math.random()*10 + 1);
 
     for (tentativa = 1; tentativa <= 3; tentativa++) {
-        let resposta = prompt(`Pensei em um número inteiro de 1 a 10, tente adivinhar qual é. Você tem ${tentativa} tentativas.`)
-        divResolucao.innerHTML += `<p>Pensei em um número inteiro de 1 a 10, tente adivinhar qual é. Você tem ${tentativa} tentativas.</p>`
+        let resposta = prompt(`Pensei em um número inteiro de 1 a 10, tente adivinhar qual é. Você tem ${4-tentativa} tentativas.`)
+        divResolucao.innerHTML += `<p>Pensei em um número inteiro de 1 a 10, tente adivinhar qual é. Você tem ${4-tentativa} tentativas.</p>`
         if (resposta == numero) {
             alert(`Parabéns, você acertou! O número era ${numero}.`);
             divResolucao.innerHTML += `<p>Parabéns, você acertou! O número era ${numero}.</p>`;
